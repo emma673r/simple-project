b661bb454bdc87fec290a6aa62b97698e47580e4
emma673r <90177139+emma673r@users.noreply.github.com>
2022-11-15 12:56:45 +0100

    thank you medssage

--- src/components/CheckOutForm.jsx
+++ src/components/CheckOutForm.jsx
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { insertOrder } from "../modules/db";
 
 function CheckOutForm(props) {
   const theForm = useRef(null);
+  const [payementCompleted, setPayementCompleted] = useState(false);
 
   async function submit(e) {
     e.preventDefault();
@@ -13,25 +14,34 @@ function CheckOutForm(props) {
       address: theForm.current.elements.address.value,
       basket: props.cart,
     });
-    console.log(response);
+
+    if (response && response.length) {
+      setPayementCompleted(true);
+    }
   }
 
   return (
-    <form onSubmit={submit} ref={theForm}>
-      <div className="form-control">
-        <label htmlFor="form-name">Name</label>
-        <input required type="text" name="name" id="form-name" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-email">Email</label>
-        <input required type="email" name="email" id="form-email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-address">Address</label>
-        <textarea required name="address" id="form-address" />
-      </div>
-      <button>Pay now</button>
-    </form>
+    <>
+      {payementCompleted ? (
+        <p>Thank you</p>
+      ) : (
+        <form onSubmit={submit} ref={theForm}>
+          <div className="form-control">
+            <label htmlFor="form-name">Name</label>
+            <input required type="text" name="name" id="form-name" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-email">Email</label>
+            <input required type="email" name="email" id="form-email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-address">Address</label>
+            <textarea required name="address" id="form-address" />
+          </div>
+          <button>Pay now</button>
+        </form>
+      )}
+    </>
   );
 }
 
